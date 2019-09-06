476172c55a50cf4a3c4a466797b3132746d7ed5e
EricVazquezDJ <eric_25_06@outlook.com>
2019-09-06 13:07:35 -0500

    Tercer version

--- Arreglos con punteros.cpp
+++ Arreglos con punteros.cpp
@@ -1,46 +1,84 @@
 #include <iostream>
 #include <conio.h>
-
+ 
 using namespace std;
-
+ 
 struct datos{
-	char nombre[20];
-	int tiempo;
-	char revista[20];
-	
+    char nombre[22];
+    int tiempo=0;
+    char revista[22];
+     
 };
-void Menu()
-	cout<<"1. Ingresar datos"<<endl;
-	cout<<"2. Eliminar datos"<<endl;
-	cout<<"3. Mostrar datos"<<endl;
-	cout<<"Elige una opcion: "<<endl;
-
-int main(int argc, char** argv){
-	
-	int opcion;
-	datos P1[3];
-	datos *apuntador=P1;
-	void Menu();
-	cin>>opcion;
-	while(repetir)
-	
-	if opcion==1{
-		for(int i=0;i<10;i++){
-		cout<<"Nombre del suscriptor: ";
-		cin.getline(P1[i].nombre,20);
-		cout<<"Tiempo de suscripcion: ";
-		cin>>P1[i].tiempo;
-		cout<<"Nombre de la revista: ";
-		cin.getline(P1[i].revista,20);
-	}else opcion==2{
-		for(;apuntador<=&P1[1];apuntador++){
-		cout<<endl<<"Nombre: "<<apuntador->nombre;
-		cout<<endl<<"Tiempo: "<<apuntador->tiempo;
-		cout<<endl<<"Revista: "<<apuntador->revista;
-	}
-	cout<<endl;
-
-	}
-	getch();
-	return 0;
+void menu(){
+    cout<<"\nMenú de opciones \n"<<endl;
+    cout<<"1 Agregar suscriptor \n"<<"2 Eliminar suscriptor \n"<<"3 Mostrar suscriptores \n"<<"4 Salir \n"<<endl;
+    cout<<"Elige una opción del menú: \n"<<endl;
+}
+ 
+int main(){
+    char *locale;
+    locale=setlocale(LC_ALL,"");
+    int repetir=true;
+    datos suscriptor[12];
+    datos *apuntador;
+    int opcion;
+    while(repetir){
+        menu();
+        cin>>opcion;
+        if(opcion==1){
+            system("CLS");
+            char nombre[22];
+            int tiempo;
+            char revista[22];
+            cout<<"Nombre del suscriptor: ";
+            fflush(stdin);
+            cin.getline(nombre,20);
+            cout<<"Tiempo de suscripcion: ";
+            cin>>tiempo;
+            fflush(stdin);
+            cout<<"Nombre de la revista: ";
+            cin.getline(revista,20);
+            apuntador=suscriptor;
+            while(apuntador->tiempo!=0) apuntador++;
+            for(int i=0;i<=20;i++) apuntador->nombre[i]=nombre[i];
+            apuntador->tiempo=tiempo;
+            for(int i=0;i<=20;i++) apuntador->revista[i]=revista[i];
+        system ("CLS");
+        }else if(opcion==2){
+            char nombre[42];
+            cout<<"Nombre de la persona: ";
+            fflush(stdin);
+            cin.getline(nombre,40);
+            apuntador=suscriptor;
+            int j,i;
+            for(i=0;i<10;i++,apuntador++){
+                for(j=0;j<40;j++){
+                    if(apuntador->nombre[j]!=nombre[j]) break;
+                }
+                if(j==40){
+                    apuntador++;
+                    for(;i<9;i++,apuntador++){
+                        if(apuntador->tiempo==0){
+                            suscriptor[i].tiempo=0;
+                            break;
+                        }
+                        for(int j=0;j<=40;j++) suscriptor[i].nombre[j]=apuntador->nombre[j];
+                        suscriptor[i].tiempo=apuntador->tiempo;
+                        for(int j=0;j<=20;i++) suscriptor[i].revista[j]=apuntador->revista[j];
+                    }
+                }
+            }
+        system("CLS");
+        }else if(opcion==3){
+            for(int i=0;suscriptor[i].tiempo!=0;i++){
+                cout<<endl<<"Nombre: "<<suscriptor[i].nombre;
+                cout<<endl<<"Tiempo: "<<suscriptor[i].tiempo;
+                cout<<endl<<"Revista: "<<suscriptor[i].revista<<endl;
+            }
+        }else cout<<"Opción inválida \n";
+        cout<<"\n¿Regresar al menú? (1 Si / 2 No)\n";
+        cin>>repetir;
+    }
+    return 0;
+    getch();
 }
